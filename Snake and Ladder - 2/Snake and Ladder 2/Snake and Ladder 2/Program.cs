fb2bee3b201b25a10c32b3fb53cfd9c729a406fd
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-04 09:49:19 +0530

    Snake and Ladder

---
+++ Snake and Ladder - 2/Snake and Ladder 2/Snake and Ladder 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_and_Ladder_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int START_POSITION =0;
+            Random rand = new Random();
+            int DICE_ROLL = rand.Next(1, 7);
+        }
+    }
+}
