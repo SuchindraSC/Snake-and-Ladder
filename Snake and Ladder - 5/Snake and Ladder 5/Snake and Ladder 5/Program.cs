24c92c7ba5e5fa9a30ea7c296f611d4589970494
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 00:26:19 +0530

    Snake And Ladder

---
+++ Snake and Ladder - 5/Snake and Ladder 5/Snake and Ladder 5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Snake_and_Ladder_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int i = 0;
+            Random rand = new Random();
+            while (position != 100)
+            {
+                ++i;
+                int diceRoll = rand.Next(1, 7);
+                int snakeLadder = rand.Next(1, 4);
+                switch (snakeLadder)
+                {
+                    case 2:
+                        Console.WriteLine("the  value of diceRoll is " + diceRoll);
+                        Console.WriteLine("the  value of snakeLadder is " + snakeLadder);
+                        position += diceRoll;
+                        Console.WriteLine("the  present position of 1st person  is " + position);
+                        break;
+                    case 3:
+                        Console.WriteLine("the  value of diceRoll is " + diceRoll);
+                        Console.WriteLine("the  value of snakeLadder is " + snakeLadder);
+                        position -= diceRoll;
+                        if (position < 0)
+                        {
+                            position = 0;
+                        }
+                        Console.WriteLine("the  present position of 1st person  is " + position);
+                        break;
+                    default:
+                        position += 0;
+                        Console.WriteLine("the value is " + position);
+                        break;
+                }
+                Console.WriteLine("The Position of the Player is "+position);
+            }
+            Console.WriteLine("As Player Reaches 100 Player Wins");
+        }
+    }
+}
